dc72979861fb54dd8875954f8d31bc7a9259ec71
davi <daviforever17@gmail.com>
2024-08-12 18:21:55 -0300

    felah meu amor

--- src/pages/ex10/index.jsx
+++ src/pages/ex10/index.jsx
@@ -26,6 +26,45 @@ function Exercicio10(){
 
             </header>
 
+            <main>
+
+                <div className='titulo-ex10'>
+
+                    <a href="/"><img className='seta' src="/assets/images/setinha_freiS.png" alt="" /></a>
+
+                <h2>Exercício 10 - Calcule o IMC</h2>
+
+                </div>
+
+                <br />
+                <hr width="94%" color='#E44F9C'/>
+
+                <div className='bloco' width="94px">
+
+                    <p>Implemente um programa em Javascript que a partir da altura e do peso de uma pessoa, calcule o IMC e avalie a faixa correspondente a tabela ao lado. Ao final, apresente o IMC e a situação.</p>
+
+                </div>
+
+                <div className='card'>
+
+                    <h3>Altura</h3>
+
+                    <input type="text"/>
+
+                    <h3>Peso</h3>
+
+                    <input type="text" />
+
+                    <button>Executar</button>
+
+                </div>
+
+                <h4>O seu IMC é: </h4>
+                <h4>Sua classificação é</h4>
+
+
+            </main>
+
         </div>
 
     )
--- src/pages/ex11/index.jsx
+++ src/pages/ex11/index.jsx
@@ -1,6 +1,27 @@
 import './index.scss'
 
+import { useState } from 'react'
+
 function Exercicio11(){
+
+    const [numero, setNumero] = useState(0);
+    const [resultados, setResultados] = useState([]);
+
+    function tabuada(){
+
+        let tb = []
+
+        for(let i = 0; i <= 10; i++){
+
+            let result = numero * i
+
+            tb[i] = `${numero} x ${i} = ${result}`   
+
+        }
+
+        setResultados(tb)
+
+    }
 
     return(
 
@@ -25,6 +46,48 @@ function Exercicio11(){
                 </div>
 
             </header>
+
+            <main>
+
+                <div className='titulo-ex11'>
+
+                    <a href="/"><img className='seta' src="/assets/images/setinha_freiS.png" alt="" /></a>
+
+                    <h2>Exercício 11 - Tabuada</h2>
+
+                </div>
+
+                <br />
+                <hr width="94%" color='#00000'/>
+
+                <div className='bloco' width="94px">
+
+                    <p>Implemente um programa em Javascript que escreva no terminal a tabuada de um número informado pelo ususário. A mensagem deve estar no formato: "A x B = X".</p>
+
+                </div>
+
+                <div className='card'>
+
+                    <h3>Tabuada do:</h3>
+
+                    <input type="text" value={numero} onChange={e => setNumero(e.target.value)}/>
+
+                    <button onClick={tabuada}>Executar</button>
+
+                </div>
+
+                <h4>{resultados.map(r => <h3> {r} </h3>)}</h4>
+
+            </main>
+
+            <section>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia error nesciunt doloribus quisquam sint dolore aliquam deleniti dignissimos repellendus. Quo laudantium at illo repudiandae nam eligendi reprehenderit incidunt vitae tempore!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam placeat sunt aliquam totam tenetur quos ea expedita cumque impedit. Sed consequuntur minima cupiditate impedit veritatis quae autem expedita? Mollitia, rem.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores odit, culpa fugiat aperiam voluptate quod ad aliquam quidem, exercitationem in animi omnis soluta. Laboriosam placeat sunt quis. Deleniti, fuga alias!
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, autem enim? Dolorem sed nisi id! Adipisci voluptas, unde accusamus officia ipsa sunt placeat doloremque perspiciatis cum facere! Reprehenderit, dolorum hic?  
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque deleniti animi itaque beatae ad autem libero quasi, necessitatibus quidem exercitationem unde eos esse saepe explicabo cupiditate mollitia ab est amet!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non molestiae hic possimus recusandae, in itaque natus. Eum eveniet nobis vel repudiandae aut corrupti, nam consequatur aliquid veniam optio maxime quibusdam!
+            </section>
 
         </div>
 
