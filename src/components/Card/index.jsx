bc1de7931e64387dc00ec7f7a1d1cabff91b2f6a
davi <daviforever17@gmail.com>
2024-08-26 18:16:04 -0300

    xxxtentacion

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,2 +1,19 @@
 import './index.scss'
 
+function Card({cor_card, titulo, subtitulo}) {
+
+    return (
+
+        <div className='card'>
+
+        <img className='retangulo' src={cor_card} alt=""/>
+      
+            <h3 >{titulo}</h3>
+
+            <p>{subtitulo}</p>
+
+        </div>
+
+    )
+
+}
